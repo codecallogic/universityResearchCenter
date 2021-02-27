665aa0afad89a3303a5f8c3cba0d085226ca13e6
Fabricio <contact@fabricioguardia.com>
2021-02-26 23:18:36 -0800

    view all table

--- pages/admin/view/[viewAll].js
+++ pages/admin/view/[viewAll].js
@@ -13,9 +13,10 @@ const ViewAll = ({loggedIn, account, allAnnouncements}) => {
 
   const [user, setUser] = useState(JSON.parse(decodeURIComponent(account)))
   const [announcements, setAnnouncements] = useState(allAnnouncements)
+  const [headers, setHeaders] = useState(Object.keys(allAnnouncements[0]))
 
   useEffect( () => {
-    console.log(announcements)
+    console.log(headers)
   }, [])
   
   return (
@@ -23,6 +24,58 @@ const ViewAll = ({loggedIn, account, allAnnouncements}) => {
     <AdminNav data={user}></AdminNav>
     <div className="announcements">
       <h1 className="announcements-header">All Announcements</h1>
+      <div className="announcements-table">
+        <div className="announcements-table-headers">
+          <div className="announcements-table-group">
+            <label htmlFor="selectAll">
+              <input type="checkbox" name="selectAll"/>
+              <span></span>
+              <div>
+                <svg>
+                  <use xlinkHref="/sprite.svg#icon-checkmark"></use>
+                </svg>
+              </div>
+            </label>
+          </div>
+          {headers !== null && headers.map( (header) => (
+          <div className="announcements-table-headers-heading">
+            {header !== '__v' ? header : null}
+            {/* TODO: Add filtering if client needs its */}
+            {header !== '__v' ?
+            <svg>
+              <use xlinkHref={`/sprite.svg#icon-chevron-thin-desc`}></use>
+            </svg>
+            : null
+            }
+          </div>
+          ))
+          }
+        </div>
+        {allAnnouncements !== null && allAnnouncements.map( (announcement) => (
+        <div className="announcements-table-rows">
+          <div className="announcements-table-group">
+            <label htmlFor="selectAll">
+              <input type="checkbox" name="selectAll"/>
+              <span></span>
+              <div>
+                <svg>
+                  <use xlinkHref="/sprite.svg#icon-checkmark"></use>
+                </svg>
+              </div>
+            </label>
+          </div>
+          {Object.keys(announcement).map( (keyName, keyIndex) => (
+          <div className="announcements-table-rows-content">
+            <span>{keyName !== '__v' ? announcement[keyName].toString().length > 50 ?  announcement[keyName].toString().substring(0, 50): announcement[keyName].toString() : null}
+            </span>
+            {/* <svg>
+              <use xlinkHref={`/sprite.svg#icon-chevron-thin-desc`}></use>
+            </svg> */}
+          </div>
+          ))}
+        </div>
+        ))}
+      </div>
     </div>
     </>
   )
