2df03bf93a94ff0f44806b9ed9696af6362173d1
Fabricio <contact@fabricioguardia.com>
2021-03-02 15:32:07 -0800

    homepage

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,43 @@
+import React, {useState, useEffect} from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
+import axios from 'axios'
+import {API} from '../config'
 
-export default function Home() {
+const Home = ({announcements}) => {
+  
+  const [announcementsList, setAnnouncementsList] = useState(announcements)
+  
   return (
     <>
     <Nav></Nav>
-    <div className="homepage">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. At ea iusto quidem cumque perferendis, voluptates earum sapiente quasi amet eius. Repellat in consectetur quaerat eum similique maiores quisquam necessitatibus tempore.
-      Maiores in laborum natus nostrum consequuntur, quia delectus? Perspiciatis sed exercitationem quaerat placeat deleniti sequi, dolore mollitia possimus. Maxime fugiat magni voluptatem adipisci, tempore error consequuntur nesciunt corporis nemo accusamus!
-      Voluptate magni exercitationem odit recusandae excepturi optio provident maiores accusamus facere laborum consectetur earum esse, velit autem temporibus. Itaque accusamus voluptate quo, ea dolorem quae quas est quisquam vero consequuntur.
-      Voluptates sunt ipsa in explicabo laboriosam eaque iste, corporis repudiandae voluptate fuga ut perferendis quis! Sequi autem, similique eveniet totam error, aliquam quos sapiente beatae, vero veritatis sunt maiores ipsam?
-      In tenetur quas a magnam assumenda esse sint quo magni ex enim maiores earum totam, non optio. Consectetur provident cumque nemo, commodi excepturi quam dolores est obcaecati incidunt distinctio quod.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. At ea iusto quidem cumque perferendis, voluptates earum sapiente quasi amet eius. Repellat in consectetur quaerat eum similique maiores quisquam necessitatibus tempore.
-      Maiores in laborum natus nostrum consequuntur, quia delectus? Perspiciatis sed exercitationem quaerat placeat deleniti sequi, dolore mollitia possimus. Maxime fugiat magni voluptatem adipisci, tempore error consequuntur nesciunt corporis nemo accusamus!
-      Voluptate magni exercitationem odit recusandae excepturi optio provident maiores accusamus facere laborum consectetur earum esse, velit autem temporibus. Itaque accusamus voluptate quo, ea dolorem quae quas est quisquam vero consequuntur.
-      Voluptates sunt ipsa in explicabo laboriosam eaque iste, corporis repudiandae voluptate fuga ut perferendis quis! Sequi autem, similique eveniet totam error, aliquam quos sapiente beatae, vero veritatis sunt maiores ipsam?
-      In tenetur quas a magnam assumenda esse sint quo magni ex enim maiores earum totam, non optio. Consectetur provident cumque nemo, commodi excepturi quam dolores est obcaecati incidunt distinctio quod.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. At ea iusto quidem cumque perferendis, voluptates earum sapiente quasi amet eius. Repellat in consectetur quaerat eum similique maiores quisquam necessitatibus tempore.
-      Maiores in laborum natus nostrum consequuntur, quia delectus? Perspiciatis sed exercitationem quaerat placeat deleniti sequi, dolore mollitia possimus. Maxime fugiat magni voluptatem adipisci, tempore error consequuntur nesciunt corporis nemo accusamus!
-      Voluptate magni exercitationem odit recusandae excepturi optio provident maiores accusamus facere laborum consectetur earum esse, velit autem temporibus. Itaque accusamus voluptate quo, ea dolorem quae quas est quisquam vero consequuntur.
-      Voluptates sunt ipsa in explicabo laboriosam eaque iste, corporis repudiandae voluptate fuga ut perferendis quis! Sequi autem, similique eveniet totam error, aliquam quos sapiente beatae, vero veritatis sunt maiores ipsam?
-      In tenetur quas a magnam assumenda esse sint quo magni ex enim maiores earum totam, non optio. Consectetur provident cumque nemo, commodi excepturi quam dolores est obcaecati incidunt distinctio quod.
+    <div className="home-container">
+      <div className="home-announcements">
+        <div className="home-announcements-header">
+          <svg>
+            <use xlinkHref="/sprite.svg#icon-announcement"></use>
+          </svg>
+          <span>Announcements</span>
+        </div>
+        <div className="home-announcements-container">
+          {announcementsList !== null && 
+            announcementsList.slice(0,1).map( (item) => 
+              <span>{item.title}</span>
+            )
+          }
+        </div>
+      </div>
     </div>
     </>
   )
 }
+
+Home.getInitialProps = async () => {
+  const announcements = await axios.get(`${API}/announcement/public/list`)
+
+  return {
+    announcements: announcements.data
+  }
+}
+
+export default Home
